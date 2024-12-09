b1cbbc7a7478ca75c8424131f2998bc7aef88149
Raghavendran R <47601578+RaghavRamesh159@users.noreply.github.com>
2024-12-09 13:06:26 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,35 @@
-import Head from "next/head";
-import Link from "next/link";
-import Layout, { siteTitle } from "../components/layout";
-import packageJson from "next/package.json";
+import Image from "next/image";
+import {S3Client, GetObjectCommand} from "@aws-sdk/client-s3"
 
-export default function Home({}) {
+export async function getServerSideProps() {
+  try {
+    const client = new S3Client({ region: "us-west-2" });
+    const response = await client.send(
+      new GetObjectCommand({
+        Bucket: process.env.BUCKET,
+        Key: "helloworld.json",
+      }),
+    );
+    const body = await response.Body.transformToString("utf8");
+    return { props: { file: body } };
+  } catch (caught) {
+    console.error(caught);
+    return { props: { file: "there was an error" } };
+  }
+}
+
+export default function Home({file}) {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-
-      <header>
-        <h1>
-          Next.js Feature Test App for the team 1234567
-        </h1>
-        <p>This app contains a handful of pages. Each page implements a specific Next.js feature. Deploy this app. Then select a test below to check if the feature works.</p>
-        <hr />
-      </header>
-      <section>
-        <b>NextJS deployed version {packageJson.version}</b>
-      </section>
-      <hr />
-      <section>
-        <Link href={"/ssg"}>Static Site Generation (SSG)</Link><br />
-        <Link href={"/ssg-dynamic/1"}>Static Site Generation — with dynamic routes</Link><br />
-        <Link href={"/ssg-dynamic-fallback/1"}>Static Site Generation — with dynamic route fallback</Link><br />
-        <Link href={`/isr`}>Incremental Static Regeneration (ISR)</Link><br />
-        <Link href={`/ssr`}>Server Side Rendering (SSR)</Link><br />
-        <Link href={`/ssr-redirect`}>Server Side Rendering - Redirect</Link><br />
-        <Link href={`/ssr-not-found`}>Server Side Rendering - Page not found</Link><br />
-        <Link href={`/api-route`}>API Route</Link><br />
-        <Link href={`/middleware-rewrite`}>Middleware — Rewrite</Link><br />
-        <Link href={`/middleware-redirect`}>Middleware — Redirect</Link><br />
-        <Link href={`/middleware-set-header`}>Middleware — Set Header</Link><br />
-        <Link href={`/image-optimization-imported`}>Image Optimization — Imported image</Link><br />
-        <Link href={`/image-optimization-remote`}>Image Optimization — Remote image</Link><br />
-        <Link href={`/image-html-tag`}>Image using html image tag from public folder</Link><br />
-        <Link href={`/font-css-font`}>Font — CSS Font</Link><br />
-        <Link href={`/font-next-font`}>Font — @next/font</Link><br />
-        <Link href={`/page-does-not-exist`}>404 Page not found</Link><br />
-        <Link href={`/about-us`}>App Dir</Link><br />
-      </section>
-    </Layout>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>
+        Hello
+        <pre>
+          {JSON.stringify(file, null, 2)}
+        </pre>
+        {console.log(`SSR env : `)}
+        {console.log(process.env.BUCKET)}
+        {console.log(` process env : ${process.env}`)}
+      </h1>
+    </div>
   );
 }
