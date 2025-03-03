9505a1610c1bfcf51faf8df4a4101a2e62d06e3a
Raghavendran R <47601578+RaghavRamesh159@users.noreply.github.com>
2025-03-02 16:12:02 -0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,43 @@
 import "../styles/global.css";
 import "../styles/layout.css";
+import { after } from 'next/server'
+import sleep from 'sleep-promise'
+import {S3Client, GetObjectCommand} from "@aws-sdk/client-s3"
 
-export default function App({ children }) {
+export default async function App({ children }) {
+  let datafetch = async () => {
+
+    try {
+      const client = new S3Client({ region: "us-west-2" });
+      const response = await client.send(
+        new GetObjectCommand({
+          Bucket: process.env.BUCKET,
+          Key: "helloworld.json",
+        }),
+      );
+      const body = await response.Body.transformToString("utf8");
+      return { props: { file: body } };
+    } catch (caught) {
+      console.error(caught);
+      return { props: { file: `there was an error : ${caught}` } };
+    }
+  }
+  let file = await datafetch()
+  after(async () => {
+    // Execute after the layout is rendered and sent to the user
+    console.log("Sleeping after sending response")
+    await sleep(3000); // Wait for 3 seconds
+    console.log("After call completed")
+  })
   return (
     <html>
       <body>
-        {children}
+        <p>
+          layout render
+        </p>
+        {JSON.stringify(file)}
+        {/* {children} */}
+        
       </body>
     </html>
   );
